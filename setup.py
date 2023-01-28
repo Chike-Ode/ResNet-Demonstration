684e54393216e1c51c6996a7937e63d004435331
root <chikeodenigbo@gmail.com>
2023-01-28 20:06:04 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Class demonstration of the ResNet architecture',
+    author='Chike Odenigbo, Mina Arzaghi & Karthika Kumar',
+    license='MIT',
+)
